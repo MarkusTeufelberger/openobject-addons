3c1abbcf330f5a29823a0f04786b78ab719a2726
Naresh Choksy <nch@tinyerp.com>
2009-05-14 12:53:48 +0530

    bugfix:rml path

--- commission_pricelist/report/saleagent_info.py
+++ commission_pricelist/report/saleagent_info.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -29,20 +29,20 @@ class saleagent_info(report_sxw.rml_parse):
         self.localcontext.update({
             'time': time,
             'compute':self.compute,
-            
+
         })
-    
-    
+
+
     def compute(self,sale_agent_id):
         result=[];
         res=[];
         sale_obj = self.pool.get('sale.agent').read(self.cr,self.uid,[sale_agent_id])[0];
         print "SALE_OBJECT",sale_obj;
-        customes_ids = sale_obj['customer'];        
+        customes_ids = sale_obj['customer'];
         print "Customer ids::",customes_ids
         ret_list = [];
-        
-        print "IN THE COMPUTE FUNCTION",customes_ids        
+
+        print "IN THE COMPUTE FUNCTION",customes_ids
         append_list = []
         for cust_id in customes_ids:
             ret_dict = {}
@@ -53,9 +53,9 @@ class saleagent_info(report_sxw.rml_parse):
             print "before append";
             ret_dict['invoices'] = self.partner_invoices(custmer_obj);
             print "\nafter append",ret_dict;
-            ret_list.append(ret_dict);          
-        #End for        
-        
+            ret_list.append(ret_dict);
+        #End for
+
         print "Ret list of partner::",ret_list;
         return ret_list
 #       return True
@@ -65,7 +65,7 @@ class saleagent_info(report_sxw.rml_parse):
         ret_list = [];
         invoice_ids = self.pool.get('account.invoice').search(self.cr,self.uid,[('partner_id','=',partner_obj.id)]);
         print "Invoices ::",invoice_ids;
-        
+
         for invoice_id in invoice_ids:
             ret_dict = {};
             invoice = self.pool.get('account.invoice').read(self.cr,self.uid,[invoice_id])[0];
@@ -76,15 +76,15 @@ class saleagent_info(report_sxw.rml_parse):
             ret_dict['amount_total'] = invoice['amount_total'];
             ret_dict['state'] = invoice['state'];
             pricelist_obj = self.pool.get('product.pricelist').browse(self.cr,self.uid,partner_obj.property_product_pricelist[0]);
-            
-            ret_dict['comm'] = str(((int(invoice['amount_total']) * int(pricelist_obj.commrate))/100))                      
+
+            ret_dict['comm'] = str(((int(invoice['amount_total']) * int(pricelist_obj.commrate))/100))
             ret_list.append(ret_dict);
         #end for invoice_id in invoice_ids:
-        
+
         print "\n List in invoice::",ret_list;
         return ret_list;
     #end def partner_invoices(self,partner_obj):
-report_sxw.report_sxw('report.saleagent.info', 'sale.agent', 'addons/saleagent/report/saleagent_info.rml', parser=saleagent_info)
+report_sxw.report_sxw('report.saleagent.info', 'sale.agent', 'addons/commission_pricelist/report/saleagent_info.rml', parser=saleagent_info)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
