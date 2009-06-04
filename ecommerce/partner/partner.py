ce6a975706ccf64afd7cf6f18f4b049376c2406a
Naresh Choksy <nch@tinyerp.com>
2009-06-04 12:45:41 +0530

    BugFix:ecommerce module

--- ecommerce/partner/partner.py
+++ ecommerce/partner/partner.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -22,17 +22,19 @@
 
 from osv import osv, fields
 from tools.translate import _
-    
+
 import sys,os,smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
+import logging
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email.MIMEMultipart import MIMEMultipart
 from email.Header import Header
+from email.Utils import formatdate, COMMASPACE
+from email.Utils import formatdate, COMMASPACE
 from email import Encoders
-import logging        
 
 class ecommerce_partner(osv.osv):
-    
+
     _description = 'ecommerce partner'
     _name = "ecommerce.partner"
     _order = "name"
@@ -50,20 +52,20 @@ class ecommerce_partner(osv.osv):
     _defaults = {
         'active': lambda *a: 1,
     }
-  
+
     def user_get(self, cr, login=None):
-        
+
         cr.execute("select * from res_users")
         res = cr.dictfetchall()
         if(login == None):
             userdata = map(lambda x: x['user_code'], res)
         else:
             userdata = map(lambda x: x['login'], res)
-            
+
         return userdata
-    
+
     def userdata_get(self, cr, uid, email_code, check=None, context={}):
-      
+
         up_data = None
         if (check == None):
             cr.execute("select id from res_users where user_code = %s", (str(email_code), ))
@@ -78,12 +80,12 @@ class ecommerce_partner(osv.osv):
             get_data = res_users.read(cr, uid, ecommerce_user.id, [], context)
             if(check == None):
                 res_users.write(cr, uid, get_data['id'], {'active':True})
-                       
+
             up_data = res_users.read(cr, uid, ecommerce_user.id, [], context)
         return up_data
- 
+
     def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=80):
-        
+
         if not args:
             args = []
         if not context:
@@ -95,9 +97,9 @@ class ecommerce_partner(osv.osv):
         else:
             ids = self.search(cr, uid, args, limit=limit, context=context)
         return self.name_get(cr, uid, ids, context=context)
-    
+
     def address_get(self, cr, uid, ids, adr_pref=['default']):
-        
+
         cr.execute('select type,id from ecommerce_partner_address where partner_id in ('+','.join(map(str, ids))+')')
         res = cr.fetchall()
         adr = dict(res)
@@ -106,18 +108,18 @@ class ecommerce_partner(osv.osv):
             default_address = adr.get('default', res[0][1])
         else:
             default_address = False
-        
+
         for a in adr_pref:
             result[a] = adr.get(a, default_address)
         return result
-    
+
     def delivery_grid(self, cr, uid, shop_id, adr_dict, context={}):
-      
+
         delivery_grid_ids = []
-        obj_shop = self.pool.get('ecommerce.shop') 
+        obj_shop = self.pool.get('ecommerce.shop')
         if(adr_dict['type'] == 'delivery'):
             add_id = adr_dict['address_id']
-           
+
         if(not adr_dict['type'] == 'delivery') and (adr_dict['type'] == 'default'):
             add_id = adr_dict['address_id']
 
@@ -126,12 +128,12 @@ class ecommerce_partner(osv.osv):
         for i in delivery_ecommerce_car[0].delivery_ids:
             delivery_grid_ids.append(i.id)
 
-        grid_id = self.grid_get(cr, uid, delivery_grid_ids, add_id, {}, from_web=True)       
+        grid_id = self.grid_get(cr, uid, delivery_grid_ids, add_id, {}, from_web=True)
         get_data = delivery_carrier.read(cr, uid, grid_id, ['name'], context)
         return get_data
 
     def grid_get(self, cr, uid, ids, contact_id, context={}, from_web=False):
-      
+
         add_grid_list = []
         obj_ecom_prt_add = self.pool.get('res.partner.address')
         if from_web:
@@ -143,7 +145,7 @@ class ecommerce_partner(osv.osv):
                         get_id = lambda x: x.id
                         country_ids = map(get_id, grid.country_ids)
                         state_ids = map(get_id, grid.state_ids)
-                       
+
                         if country_ids and not contact.country_id.id in country_ids:
                             continue
                         if state_ids and not contact.state_id.id in state_ids:
@@ -153,9 +155,9 @@ class ecommerce_partner(osv.osv):
                         if grid.zip_to and (contact.zip or '') > grid.zip_to:
                             continue
                         add_grid_list.append(grid.id)
-                    
+
                 return add_grid_list
-        else:    
+        else:
             contact = obj_ecom_prt_add.browse(cr, uid, [contact_id])[0]
             for carrier in self.browse(cr, uid, ids):
                 for grid in carrier.grids_id:
@@ -172,19 +174,19 @@ class ecommerce_partner(osv.osv):
                         continue
                     return grid.id
             return False
-  
-        
+
+
     def delivery_get_price(self, cr, uid, grid_id, product_list, context):
-         
+
         prd_list_ids = []
         total = 0
         weight = 0
         volume = 0
         final_tax_amt = 0
-       
+
         tax_obj = self.pool.get('account.tax')
         for prd_ids in product_list:
-            prd_list_ids.append(prd_ids['id']) 
+            prd_list_ids.append(prd_ids['id'])
         for prd in product_list:
             p = self.pool.get('product.product').browse(cr, uid, prd['id'])
             sub_total = round(prd['price'] * prd['quantity'])
@@ -197,13 +199,13 @@ class ecommerce_partner(osv.osv):
                 for tax in tax_obj.compute(cr, uid, p.taxes_id, prd['price'], prd['quantity'], product=prd['id']):
                     final_tax_amt += tax['amount']
         get_ship_price = self.get_price_from_picking_ecommerce(cr, uid, grid_id, total, weight, volume, context)
-        
-        return dict(get_ship_price=get_ship_price, final_tax_amt=final_tax_amt)  
-           
 
-    
+        return dict(get_ship_price=get_ship_price, final_tax_amt=final_tax_amt)
+
+
+
     def get_price_from_picking_ecommerce(self, cr, uid, get_id, total, weight, volume, context={}):
-        
+
         grid = self.pool.get('delivery.grid')
         grid_get = grid.browse(cr, uid, int(get_id))
         price = 0.0
@@ -222,12 +224,12 @@ class ecommerce_partner(osv.osv):
         if not ok:
             raise osv.except_osv(_('No price avaible !'),
                                   _('No line matched this order in the choosed delivery grids !'))
-        return price   
-    
+        return price
+
     def ecommerce_sendmail(self, cr, uid, mail_to, subject, body, attachment=None, context = {}):
         try:
             mail_from = 'mansuri.sananaz@gmail.com'
-            
+
             s = smtplib.SMTP('smtp.gmail.com', 587)
             s.set_debuglevel(1)
             s.ehlo()
@@ -239,11 +241,11 @@ class ecommerce_partner(osv.osv):
             outer['To'] = mail_to
             outer['From'] = "noreply"
             outer.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-          
+
             msg = MIMEText(body or '',  _charset = 'utf-8')
-         
+
             if(attachment == None):
-                
+
                 msg['Subject'] = Header(subject.decode('utf8'), 'utf-8')
                 msg['From'] = "noreply"
                 s.sendmail(mail_from, mail_to, msg.as_string())
@@ -251,19 +253,19 @@ class ecommerce_partner(osv.osv):
                 msg.set_payload(attachment)
                 Encoders.encode_base64(msg)
                 msg.add_header('Content-Disposition', 'attachment', filename='invoice.pdf')
-               
+
                 outer.attach(msg)
                 outer.attach(MIMEText(body))
 
                 composed = outer.as_string()
                 s.sendmail(mail_from, mail_to, composed)
             s.close()
-        
+
         except Exception, e:
             logging.getLogger().error(str(e))
-        
-        return True 
-        
+
+        return True
+
 ecommerce_partner()
 
 class ecommerce_partner_address(osv.osv):
