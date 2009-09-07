10f6ecf08a1119ac43bb7ad8d0a6035f3f463099
Christophe Chauvet <christophe.chauvet@syleam.fr>
2009-09-07 22:41:45 +0200

    [FIX] Bad licence version, thanks for the report

--- point_of_sale_extension/company.py
+++ point_of_sale_extension/company.py
@@ -1,33 +1,28 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    point_of_sale_extension module for OpenERP, profile for 2ed customer
 #
-# Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
-#                    Jordi Esteve <jesteve@zikzakmedia.com>
-# Copyright (c) 2009 SYLEAM (http://syleam.fr) Al Rights Reserved
-#                    Christophe Chauvet <christophe.chauvet@syleam.fr>
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) 
+#       All Rights Reserved, Jordi Esteve <jesteve@zikzakmedia.com>
+#    Copyright (c) 2009 SYLEAM (http://syleam.fr) 
+#       All Rights Reserved, Christophe Chauvet <christophe.chauvet@syleam.fr>
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This file is a part of point_of_sale_extension
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    point_of_sale_extension is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#    point_of_sale_extension is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
