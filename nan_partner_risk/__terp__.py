e1b4d494b237c4d7bb83cef474f22fe034aa2fc3
Albert Cervera i Areny <albert@nan-tic.com>
2009-09-09 15:43:45 +0200

    Added new "Partner Risk Analisis" module.

---
+++ nan_partner_risk/__terp__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+{
+	"name" : "Partner Risk Analysis",
+	"version" : "0.1",
+	"description" : """This module adds a new button in the partner form to analyze current state of a partner risk.
+It reports current information regarding amount of debt in invoices, orders, etc.""",
+	"author" : "NaN",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 
+		'base', 
+		'account', 
+		'sale',
+		'account_payment',
+	], 
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [
+		'risk_view.xml',
+	],
+	"active": False,
+	"installable": True
+}
