3ee7c3236f9d40f12cb2a6ee04fb635782481c91
Thorsten Vocks <>
2009-10-28 12:58:28 +0530

    Merged module: l10n_chart-de_skr03

--- l10n_chart_de_skr03/__terp__.py
+++ l10n_chart_de_skr03/__terp__.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution	
-#    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -19,18 +19,68 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{
-    "name" : "German - SKR03 Chart of Accounts",
-    "version" : "1.0",
-    "author" : "Tiny",
-    "category" : "Localisation/Account Charts",
-    "description": "This is the module to manage the accounting chart for Germany (SKR03 Profile) in Open ERP.",
-    "depends" : ["base_iban", "base_vat", "account_chart"],
-    "demo_xml" : [],
-    "update_xml" : ['account_tax_code.xml',"account_chart.xml",
-                    'account_tax.xml','l10n_chart_de_skr03_wizard.xml'],
-    "active": False,
-    "installable": True
-}
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
+# Dieses Modul bietet Ihnen einen deutschen Kontenplan basierend auf dem SKR03.
+# Gemäss der aktuellen Einstellungen ist die Firma nicht Umsatzsteuerpflichtig.
+# Diese Grundeinstellung ist sehr einfach zu ändern und bedarf in der Regel
+# grundsätzlich eine initiale Zuweisung von Steuerkonten zu Produkten und / oder 
+# Sachkonten oder zu Partnern.
+# Die Umsatzsteuern (voller Steuersatz, reduzierte Steuer und steuerfrei)
+# sollten bei den Produktstammdaten hinterlegt werden (in Abhängigkeit der
+# Steuervorschriften). Die Zuordnung erfolgt auf dem Aktenreiter Finanzbuchhaltung
+# (Kategorie: Umsatzsteuer).
+# Die Vorsteuern (voller Steuersatz, reduzierte Steuer und steuerfrei)
+# sollten ebenso bei den Produktstammdaten hinterlegt werden (in Abhängigkeit 
+# der Steuervorschriften). Die Zuordnung erfolgt auf dem Aktenreiter 
+# Finanzbuchhaltung (Kategorie: Vorsteuer).
+# Die Zuordnung der Steuern für Ein- und Ausfuhren aus EU Ländern, sowie auch
+# für den Ein- und Verkauf aus und in Drittländer sollten beim Partner 
+# (Lieferant/Kunde)hinterlegt werden (in Anhängigkeit vom Herkunftsland 
+# des Lieferanten/Kunden). Die Zuordnung beim Kunden ist "höherwertig" als 
+# die Zuordnung bei Produkten und überschreibt diese im Einzelfall.
+#
+# Zur Vereinfachung der Steuerausweise und Buchung bei Auslandsgeschäften 
+# erlaubt OpenERP ein generelles Mapping von Steuerausweis und Steuerkonten
+# (z.B. Zuordnung "Umsatzsteuer 19%" zu "steuerfreie Einfuhren aus der EU") 
+# zwecks Zuordnung dieses Mappings zum ausländischen Partner (Kunde/Lieferant).
+
+# Die Rechnungsbuchung beim Einkauf bewirkt folgendes:
+# Die Steuerbemessungsgrundlage (exklusive Steuer) wird ausgewiesen bei den 
+# jeweiligen Kategorien für den Vorsteuer Steuermessbetrag (z.B. Vorsteuer
+# Steuermessbetrag Voller Steuersatz 19%).
+# Der Steuerbetrag erscheint unter der Kategorie "Vorsteuern" (z.B. Vorsteuer
+# 19%). Durch multidimensionale Hierachien können verschiedene Positionen
+# zusammengefasst werden und dann in Form eines Reports ausgegeben werden.
+#
+# Die Rechnungsbuchung beim Verkauf bewirkt folgendes:
+# Die Steuerbemessungsgrundlage (exklusive Steuer) wird ausgewiesen bei den 
+# jeweiligen Kategorien für den Umsatzsteuer Steuermessbetrag 
+# (z.B. Umsatzsteuer Steuermessbetrag Voller Steuersatz 19%).
+# Der Steuerbetrag erscheint unter der Kategorie "Umsatzsteuer" 
+# (z.B. Umsatzsteuer 19%). Durch multidimensionale Hierachien können 
+# verschiedene Positionen zusammengefasst werden.
+# Die zugewiesenen Steuerausweise können auf Ebene der einzelnen
+# Rechnung (Eingangs- und Ausgangsrechnung) nachvollzogen werden,
+# und dort gegebenenfalls angepasst werden.
+
+# Rechnungsgutschriften führen zu einer Korrektur (Gegenposition)
+# der Steuerbuchung, in Form einer spiegelbildlichen Buchung.
+{
+        "name" : "Deutschland - SKR03",
+        "version" : "1.0",
+        "author" : "openbig.org",
+        "website" : "http://www.openbig.org",
+        "category" : "Localisation/Account Charts",
+        "description": """Dieses  Modul beinhaltet einen deutschen Kontenrahmen basierend auf dem SKR03.""",
+        "depends" : ['base', 'account', 'base_iban', 'base_vat', 'account_chart'],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        "update_xml" : [
+        "account_types.xml",
+        "account_chart.xml",
+        "account_tax.xml",
+	"account_tax_fiscal_position.xml",
+        "l10n_de_skr03_wizard.xml",
+        ],
+     "installable": True
+} 
