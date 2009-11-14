7fede23af406448b38185f4d03b371e9c4416dba
Sebastien LANGE <sebastien.lange@Syleam.fr>
2009-11-14 19:02:13 +0100

    [IMP] hr_attendance_project : add choice task and Open it in start work add link in task project

--- hr_attendance_project/wizard/sign_in_out_task_project.py
+++ hr_attendance_project/wizard/sign_in_out_task_project.py
@@ -5,6 +5,8 @@
 #    Copyright (C) 2008 Zikzakmedia S.L. (http://zikzakmedia.com)
 #       Raimon Esteve <resteve@zikzakmedia.com> All Rights Reserved.
 #       Jordi Esteve <resteve@zikzakmedia.com> All Rights Reserved.
+#    Copyright (C) 2009 SYLEAM Info Services (http://www.Syleam.fr)
+#       Sebastien LANGE <sebastien.lange@syleam.fr> All Rights Reserved.
 #
 #    This file is a part of hr_attendance_project
 #
@@ -39,6 +41,7 @@ si_form ='''<?xml version="1.0"?>
     <label string="(local time on the server side)" colspan="2"/>
     <field name="date"/>
     <label string="(Keep empty for current time)" colspan="2"/>
+    <field name="tasks_project" domain="[('project_id.members', 'child_of', [uid]),('state','in',['draft','open'])]" colspan="4"/>
 </form>'''
 
 si_fields = {
@@ -46,6 +49,7 @@ si_fields = {
     'state': {'string': "Current state", 'type' : 'char', 'required' : True, 'readonly': True},
     'date': {'string':"Starting Date", 'type':'datetime'},
     'server_date': {'string':"Current Date", 'type':'datetime', 'readonly':True},
+    'tasks_project': {'string':"Task", 'type':'many2one', 'relation':'project.task'},
 }
 
 so_form_base = '''<?xml version="1.0" ?>
@@ -57,7 +61,7 @@ so_form_base = '''<?xml version="1.0" ?>
     <field name="server_date"/>
     <separator string="Work done in the last period" colspan="4" />
     <field name="account_id" colspan="2" attrs="{'readonly':[('project_id','!=',False)],'required':[('project_id','==',False)]}"/>
-    <field name="project_id" attrs="{'readonly':[('account_id','!=',False)],'required':[('account_id','==',False)]}" />
+    <field name="project_id" attrs="{'readonly':[('account_id','!=',False)],'required':[('account_id','==',False)]}" domain="[('members','child_of',[uid]), ('state','=','open')]" />
     <field name="tasks_account" domain="[('project_id.category_id', '=', account_id),('state','=','open'),%s]" attrs="{'readonly':[('project_id','!=',False)],'required':[('project_id','==',False)]}"/>
     <field name="tasks_project" domain="[('project_id', '=', project_id),('state','=','open'),%s]" attrs="{'readonly':[('account_id','!=',False)],'required':[('account_id','==',False)]}"/>
     <field name="info" colspan="4"/>
@@ -90,15 +94,20 @@ def _get_empid(self, cr, uid, data, context):
         return {'name': employee['name'], 'state': employee['state'], 'emp_id': emp_id[0], 'date':False, 'server_date':time.strftime('%Y-%m-%d %H:%M:%S')}
     raise wizard.except_wizard(_('UserError'), _('No employee defined for your user !'))
 
-def _get_empid2(self, cr, uid, data, context):
+def _get_empid2(self, cr, uid, data, context=None):
+    if not context:
+        context = {}
     model_data_obj = pooler.get_pool(cr.dbname).get('ir.model.data')
     menu_id = model_data_obj.search(cr, uid, [('name','=','menu_sitp_sotp_my')])[0] 
     menu = model_data_obj.browse(cr, uid, menu_id)
-    if menu.res_id == data['id']:
-        # The menu 'Sign in / Sign out by my task project' has been selected
-        so_form.string = so_form_base % ("('user_id', '=', uid)", "('user_id', '=', uid)")
+    if not context.get('task', False):
+        if menu.res_id == data['id']:
+            # The menu 'Sign in / Sign out by my task project' has been selected
+            so_form.string = so_form_base % ("('user_id', '=', uid)", "('user_id', '=', uid)")
+        else:
+            # The menu 'Sign in / Sign out by task project' has been selected
+            so_form.string = so_form_base % ('', '')
     else:
-        # The menu 'Sign in / Sign out by task project' has been selected
         so_form.string = so_form_base % ('', '')
 
     res = _get_empid(self,cr, uid, data, context)
@@ -107,10 +116,16 @@ def _get_empid2(self, cr, uid, data, context):
     res['date_start'] = cr.fetchone()[0]
     res['info'] = ''
     res['account_id'] = False
+    if context.get('task', False):
+        task_obj = pooler.get_pool(cr.dbname).get('project.task')
+        task = task_obj.browse(cr, uid, data['id'])
+        res['project_id'] = task.project_id.id
+        res['tasks_project'] = data['id']
     return res
 
 def _sign_in_result(self, cr, uid, data, context):
-    emp_obj = pooler.get_pool(cr.dbname).get('hr.employee')
+    pool_obj = pooler.get_pool(cr.dbname)
+    emp_obj = pool_obj.get('hr.employee')
     emp_id = data['form']['emp_id']
     from osv.osv import except_osv
     try:
@@ -118,6 +133,31 @@ def _sign_in_result(self, cr, uid, data, context):
     except except_osv, e:
         raise wizard.except_wizard(e.name, e.value)
     return {}
+
+def _open_task(self, cr, uid, data, context):
+    pool_obj = pooler.get_pool(cr.dbname)
+    emp_obj = pool_obj.get('hr.employee')
+    emp_id = data['form']['emp_id']
+    model_data_ids = pool_obj.get('ir.model.data').search(cr,uid,[('model','=','ir.ui.view'),('name','=','view_task_form2')])
+    resource_id = pool_obj.get('ir.model.data').read(cr,uid,model_data_ids,fields=['res_id'])[0]['res_id']
+    from osv.osv import except_osv
+    try:
+        success = emp_obj.sign_in(cr, uid, [emp_id], dt=data['form']['date'] or False)
+    except except_osv, e:
+        raise wizard.except_wizard(e.name, e.value)
+    if data['form']['tasks_project']:
+        return {
+            'domain': "[('id','=', %s)]" %  data['form']['tasks_project'],
+            'context': "{'task':1}",
+            'name': 'Task',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'res_model': 'project.task',
+            'views': [(False,'tree'),(resource_id,'form')],
+            'type': 'ir.actions.act_window'
+        }
+    else:
+        return {}
 
 def _write(self, cr, uid, data, emp_id, context):
     project_obj = pooler.get_pool(cr.dbname).get('project.project')
@@ -192,11 +232,15 @@ class wiz_sitp_sotp(wizard.interface):
             },
             'sign_out' : { # this means sign_in...
                 'actions' : [_get_empid],
-                'result' : {'type':'form', 'arch':si_form, 'fields' : si_fields, 'state':[('end', 'Cancel'),('si_result', 'Start Working') ] }
+                'result' : {'type':'form', 'arch':si_form, 'fields' : si_fields, 'state':[('end', 'Cancel'),('si_result', 'Start Working'),('si_result_open_task', 'Start Working and Open Task') ] }
             },
             'si_result' : {
                 'actions' : [_sign_in_result],
                 'result' : {'type':'state', 'state':'end'}
+            },
+            'si_result_open_task': {
+                'actions': [],
+                'result': {'type':'action', 'action':_open_task, 'state':'end'}
             },
             'sign_in' : { # this means sign_out...
                 'actions' : [_get_empid2],
