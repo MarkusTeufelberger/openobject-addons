c38c7a2a3fff03579e6b665aba3ba768a7a3da9e
nicolas.bessi@camptocamp.com <>
2009-11-20 16:00:44 +0100

    some fixes in c2c modules

--- c2c_invoice_report/report/invoice.py
+++ c2c_invoice_report/report/invoice.py
@@ -30,6 +30,9 @@ from mx.DateTime import *
 from report import report_sxw
 import xml
 import pdb
+import tools
+
+
 #
 #
 #
@@ -50,23 +53,32 @@ class account_invoice_stdc2c(report_sxw.rml_parse):
     _name = 'report.account_invoice_stdc2c'
     
     def __init__(self, cr, uid, name, context=None):
+        
+        # tools.debug(name)
+        
         super(account_invoice_stdc2c, self).__init__( cr, uid, name, context)
         self.localcontext.update({
             'time': time,
             'format_date': self._get_and_change_date_format_for_swiss,
         })
         
-        
+
     def _get_and_change_date_format_for_swiss (self,date_to_format):
+        tools.debug(date_to_format)
         date_formatted=''
         if date_to_format:
             date_formatted = strptime (date_to_format,'%Y-%m-%d').strftime('%d.%m.%Y')
+        tools.debug(date_to_format)
         return date_formatted
         
+    # 
+    # def formatLang(self,*args,**kwargs):
+    #     tools.debug(args)
+    #     return super(account_invoice_stdc2c).formatLang(*args,**kwargs)
+
 report_sxw.report_sxw(
-                        'report.account_invoice_stdc2c', 
-                        'account.invoice', 
-                        'addons/c2c_standard_report/report/invoice.rml', 
+                        'report.account.invoicec2c',
+                        'account.invoice',
+                        'addons/c2c_invoice_report/report/invoice.rml',
                         parser=account_invoice_stdc2c
                     )
-
