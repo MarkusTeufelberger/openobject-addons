66fab9e7374047817c5c454233812f0adb0528af
eda (Axelor) <eda@openerp.co.in>
2009-12-08 19:06:31 +0530

    [IMP] stock_routings : Give effect in routes planned date as picking date is changed

--- stock_routings/stock/stock.py
+++ stock_routings/stock/stock.py
@@ -27,6 +27,7 @@
 from osv import osv, fields
 import netsvc
 import time
+import datetime
 from mx import DateTime
 import ir
 from tools import config
@@ -104,13 +105,27 @@ class stock_picking(osv.osv):
                 'min_date': fields.function(get_min_max_date, fnct_inv=_set_minimum_date, multi="min_max_date",
                  method=True,store=True, type='date', string='Planned Date', select=1),
                 'sequence_id': fields.many2one('ir.sequence','Sequence'),
+                'picking_date' : fields.date('Picking Date', help="Date when actual picking is done by user.",states={'done':[('readonly',True)]}),
+                'system_date' : fields.datetime('System Date',states={'done':[('readonly',True)]}),
               }
 stock_picking()
+
+def date_difference(cr,uid,old_date,new_date):
+    final_old_dt=old_date.split(' ')
+    final_new_dt=new_date.split(' ')
+    old_dt=final_old_dt[0].split('-')
+    new_dt=final_new_dt[0].split('-')
+    old_yy,old_mm,old_dd=old_dt[0],old_dt[1],old_dt[2]
+    new_yy,new_mm,new_dd=new_dt[0],new_dt[1],new_dt[2]
+    days_delay=datetime.date(int(new_yy),int(new_mm),int(new_dd))-datetime.date(int(old_yy),int(old_mm),int(old_dd))
+    
+    return days_delay.days
 
 class stock_move(osv.osv):
     _inherit="stock.move"
     
     _columns={
+              'picking_date' : fields.date('Picking Date'),
               'state': fields.selection([('draft','Draft'),('waiting','Waiting'),('confirmed','Confirmed'),('assigned','Available'),('done','Received'),('cancel','Canceled')], 'Status', readonly=True, select=True),
               'product_qty': fields.float('Quantity', required=True,states={'confirmed': [('readonly', True)],'assigned': [('readonly', True)],'flottant': [('readonly', True)],'underway': [('readonly', True)],'unproduction': [('readonly', True)],'cancel': [('readonly', True)]}),
 #              'date_planned': fields.date('Date', required=True, help="Scheduled date for the movement of the products or real date if the move is done."),
@@ -187,6 +202,188 @@ class stock_move(osv.osv):
                 
         create_chained_picking(self, cr, uid, n_moves, context)
         return []
+    
+    def action_done(self, cr, uid, ids, context=None):
+        track_flag = False
+        for move in self.browse(cr, uid, ids):
+            if move.move_dest_id.id and (move.state != 'done'):
+                cr.execute('insert into stock_move_history_ids (parent_id,child_id) values (%s,%s)', (move.id, move.move_dest_id.id))
+                if move.move_dest_id.state in ('waiting', 'confirmed'):
+                    self.write(cr, uid, [move.move_dest_id.id], {'state': 'assigned'})
+                    if move.move_dest_id.picking_id:
+                        wf_service = netsvc.LocalService("workflow")
+                        wf_service.trg_write(uid, 'stock.picking', move.move_dest_id.picking_id.id, cr)
+                    else:
+                        pass
+                        # self.action_done(cr, uid, [move.move_dest_id.id])
+                    if move.move_dest_id.auto_validate:
+                        self.action_done(cr, uid, [move.move_dest_id.id], context=context)
+
+            #
+            # Accounting Entries
+            #
+            acc_src = None
+            acc_dest = None
+            if move.location_id.account_id:
+                acc_src = move.location_id.account_id.id
+            if move.location_dest_id.account_id:
+                acc_dest = move.location_dest_id.account_id.id
+            if acc_src or acc_dest:
+                test = [('product.product', move.product_id.id)]
+                if move.product_id.categ_id:
+                    test.append( ('product.category', move.product_id.categ_id.id) )
+                if not acc_src:
+                    acc_src = move.product_id.product_tmpl_id.\
+                            property_stock_account_input.id
+                    if not acc_src:
+                        acc_src = move.product_id.categ_id.\
+                                property_stock_account_input_categ.id
+                    if not acc_src:
+                        raise osv.except_osv(_('Error!'),
+                                _('There is no stock input account defined ' \
+                                        'for this product: "%s" (id: %d)') % \
+                                        (move.product_id.name,
+                                            move.product_id.id,))
+                if not acc_dest:
+                    acc_dest = move.product_id.product_tmpl_id.\
+                            property_stock_account_output.id
+                    if not acc_dest:
+                        acc_dest = move.product_id.categ_id.\
+                                property_stock_account_output_categ.id
+                    if not acc_dest:
+                        raise osv.except_osv(_('Error!'),
+                                _('There is no stock output account defined ' \
+                                        'for this product: "%s" (id: %d)') % \
+                                        (move.product_id.name,
+                                            move.product_id.id,))
+                if not move.product_id.categ_id.property_stock_journal.id:
+                    raise osv.except_osv(_('Error!'),
+                        _('There is no journal defined '\
+                            'on the product category: "%s" (id: %d)') % \
+                            (move.product_id.categ_id.name,
+                                move.product_id.categ_id.id,))
+                journal_id = move.product_id.categ_id.property_stock_journal.id
+                if acc_src != acc_dest:
+                    ref = move.picking_id and move.picking_id.name or False
+                    product_uom_obj = self.pool.get('product.uom')
+                    default_uom = move.product_id.uom_id.id
+                    q = product_uom_obj._compute_qty(cr, uid, move.product_uom.id, move.product_qty, default_uom)
+                    if move.product_id.cost_method == 'average' and move.price_unit:
+                        amount = q * move.price_unit
+                    else:
+                        amount = q * move.product_id.standard_price
+
+                    date = time.strftime('%Y-%m-%d')
+                    partner_id = False
+                    if move.picking_id:
+                        partner_id = move.picking_id.address_id and (move.picking_id.address_id.partner_id and move.picking_id.address_id.partner_id.id or False) or False
+                    lines = [
+                            (0, 0, {
+                                'name': move.name,
+                                'quantity': move.product_qty,
+                                'product_id': move.product_id and move.product_id.id or False,
+                                'credit': amount,
+                                'account_id': acc_src,
+                                'ref': ref,
+                                'date': date,
+                                'partner_id': partner_id}),
+                            (0, 0, {
+                                'name': move.name,
+                                'product_id': move.product_id and move.product_id.id or False,
+                                'quantity': move.product_qty,
+                                'debit': amount,
+                                'account_id': acc_dest,
+                                'ref': ref,
+                                'date': date,
+                                'partner_id': partner_id})
+                    ]
+                    self.pool.get('account.move').create(cr, uid, {
+                        'name': move.name,
+                        'journal_id': journal_id,
+                        'line_id': lines,
+                        'ref': ref,
+                    })
+        
+        stock_obj=self.pool.get('stock.picking')
+        stock_history_obj=self.pool.get('stock.history')
+        user_obj=self.pool.get('res.users')
+        user=user_obj.read(cr,uid,uid,['name'])['name']
+        current_date=time.strftime('%Y-%m-%d %H:%M:%S')
+        move_dates=self.read(cr,uid,ids,['date_planned','picking_id','move_dest_id'])
+        first_pick_date=False
+        if move_dates:
+            if move_dates[0]['picking_id']:
+                old_date=stock_obj.read(cr,uid,move_dates[0]['picking_id'][0],['min_date','picking_date'])
+                if old_date:
+                    old_planned_date=old_date['min_date']
+                    old_pick_date=old_date['picking_date']
+                    first_pick_date=old_pick_date
+                    
+        days_delay=date_difference(cr,uid,old_planned_date,old_pick_date)
+       
+        move_ids=[]
+        move_ids=map(lambda x: x ,ids)
+        all_pick_ids=[]
+        next_move_ids=[]
+        
+        for mv_id in move_ids:
+            move_dt=self.read(cr,uid,mv_id,['date_planned','picking_id','move_dest_id'])
+            
+            if not all_pick_ids.__contains__(move_dt['picking_id'][0]) :
+                
+                if move_dt['picking_id'][0]!=move_dates[0]['picking_id'][0]:
+                    all_pick_ids.append(move_dt['picking_id'][0])
+            if move_dt['move_dest_id']:
+                move_ids.append(move_dt['move_dest_id'][0])
+                next_move_ids.append(move_dt['move_dest_id'][0])
+        
+        for next_mv_id in next_move_ids:
+            next_move_dt=self.read(cr,uid,next_mv_id,['date_planned','picking_id','move_dest_id'])
+            new_move_date=(DateTime.strptime(next_move_dt['date_planned'], '%Y-%m-%d %H:%M:%S') + DateTime.RelativeDateTime(days=days_delay or 0)).strftime('%Y-%m-%d %H:%M:%S')
+            self.write(cr,uid,next_move_dt['id'],{'date_planned':new_move_date})
+        
+        res=[]
+        if all_pick_ids:
+            cr.execute("""select
+                    picking_id,
+                    min(date_planned),
+                    max(date_planned)
+                from
+                    stock_move
+                where
+                    picking_id in (""" + ','.join(map(str, all_pick_ids)) + """)
+                group by
+                    picking_id""")
+            for pick, dt1,dt2 in cr.fetchall():
+               
+                r={}
+                r['id']=pick
+                r['min_date'] = dt1
+                r['max_date'] = dt2
+                res.append(r)
+            
+            for r in res:
+                old_pick_data=stock_obj.read(cr,uid,r['id'],['min_date','state'])
+                old_pick_date=old_pick_data['min_date']
+                state=old_pick_data['state']
+                if state<>'done':
+                    vals={}
+                    vals['date']=time.strftime('%Y-%m-%d')
+                    vals['prev_plan_date']=old_pick_date
+                    vals['new_plan_date']=r['min_date']
+                    vals['user']=user
+                    vals['history_id']=r['id']
+                    history_id=stock_history_obj.create(cr,uid,vals,context=context)
+                    
+                    stock_obj.write(cr,uid,r['id'],{'min_date':r['min_date']}) 
+             
+        self.write(cr, uid, ids, {'state': 'done','picking_date':first_pick_date})
+        stock_obj.write(cr,uid,move_dates[0]['picking_id'][0],{'system_date': time.strftime('%Y-%m-%d %H:%M:%S')})
+        wf_service = netsvc.LocalService("workflow")
+        for id in ids:
+            wf_service.trg_trigger(uid, 'stock.move', id, cr)
+        return True
+    
 stock_move()
 
 class stock_routing(osv.osv):
