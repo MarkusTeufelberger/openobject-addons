3cde78bec28ca52999c6aa1f2b4df4cf90899cbb
sebastien.beau <sebastien.beau@akretion.com.br>
2009-12-21 19:49:08 -0200

    [IMP] add on change filters

--- crm.py
+++ crm.py
@@ -1,3 +1,25 @@
+# -*- encoding: utf-8 -*-
+#########################################################################
+#This module intergrates Open ERP with the magento core                 #
+#Core settings are stored here                                          #
+#########################################################################
+#                                                                       #
+# Copyright (C) 2009  Raphaël Valyi                                     #
+#                                                                       #
+#This program is free software: you can redistribute it and/or modify   #
+#it under the terms of the GNU General Public License as published by   #
+#the Free Software Foundation, either version 3 of the License, or      #
+#(at your option) any later version.                                    #
+#                                                                       #
+#This program is distributed in the hope that it will be useful,        #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#GNU General Public License for more details.                           #
+#                                                                       #
+#You should have received a copy of the GNU General Public License      #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################
+
 from osv import fields, osv
 import tools
 import ir
@@ -14,14 +36,27 @@ class crm_case(osv.osv):
         'related_outgoing_picking_state': fields.related('outgoing_picking_id', 'state', type="char", string="Related Picking State", readonly=True),
         'in_supplier_picking_id': fields.many2one('stock.picking', 'Return To Supplier Picking', required=False, select=True),
         'out_supplier_picking_id': fields.many2one('stock.picking', 'Return From Supplier Picking', required=False, select=True),
-        'prodlot_id': fields.many2one('stock.production.lot', 'Serial Number'),
+        'prodlot_id': fields.many2one('stock.production.lot', 'Serial / Lot Number'),
         'product_id': fields.many2one('product.product', 'Product'),
-        'invoice_id': fields.many2one('account.invoice', 'Invoice'),
+        'invoice_id': fields.many2one('account.invoice', 'Invoice')
     }
     
     _defaults = {
         'rma_ref': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'rma'),
     }
-         
-        
+
+
+    def onchange_form(self, cr, uid, ids, partner_id=False, invoice_id=False):
+        result = {}
+        if partner_id:           
+            result = {'value':{'invoice_id': False, 'product_id': False, 'prodlot_id': False}, 'domain':{'invoice_id':"[('partner_id','=',partner_id)]"}}
+        if invoice_id:
+            inv = self.pool.get('account.invoice').browse(cr, uid, invoice_id)
+            inv_lines = inv.invoice_line
+            product_ids =[]
+            for inv_line in inv_lines:
+                product_ids.append(self.pool.get('account.invoice.line').read(cr, uid, inv_line.id)['product_id'][0])
+            result = {'value':{'partner_id': inv.partner_id.id, 'product_id':False, 'prodlot_id': False}, 'domain':{'product_id':"[('id','in', %s)]" % product_ids}}   
+        return result
+
 crm_case()
