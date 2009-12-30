af2cda1b639572758aa84d24acae22cfd2b76dbb
Albert Cervera i Areny <albert@nan-tic.com>
2009-12-30 16:54:50 +0100

    [IMP] stock_delivery_packages: Created new module that simply adds a field to introduce the number of packages that contain an outgoing picking.

---
+++ stock_delivery_packages/__terp__.py
@@ -0,0 +1,18 @@
+{
+	"name" : "Stock Delivery Packages",
+	"version" : "0.1",
+	"description" : """This module adds a new field to outgoing pickings to store the number of packges to be delivered.""",
+	"author" : "NaN",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 
+		'delivery',
+	],
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [ 
+		'stock_view.xml',
+	],
+	"active": False,
+	"installable": True
+}
