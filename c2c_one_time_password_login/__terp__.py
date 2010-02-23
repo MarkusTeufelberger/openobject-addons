f2d488b93ea1b18a01fc4589682695716bdc3a9f
nicolas.bessi@camptocamp.com <>
2010-02-23 13:33:42 +0100

    addog OTP modules documentation

--- c2c_one_time_password_login/__terp__.py
+++ c2c_one_time_password_login/__terp__.py
@@ -7,8 +7,19 @@
 {
     "name" : "c2c_one_time_password_login",
     "version" : "1.0",
-    "description":""" Generic OTP login module that provide basic 
-    session and mechanisme for OTP
+    "description":""" Generic OTP login module that provides basic 
+    session and mechanism for OTP.
+    
+    We do not intend to provide a generic framework for authentication but just one for OTP.   
+    Actually it is not design to support CAS, Kerberos etc.
+
+    The c2c_one_time_password_login provides the base mechanism for OTP 
+    like authentication function overriding, user session, login management, timeout management, mutli connection, etc.
+    You have one function to override in a provider module. The function is check_otp.
+    A provider module let you to write a provider for a specific OTP system 
+    You have a sample in the c2c_one_time_password_login_yubikey_provider module.
+    The OTP configuration is done on the company view. Please see the help in the form. 
+    
         """,
     "category" : "security",
 
