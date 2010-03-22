0ef3a8b746356ad8f7ded6d308470e53f5913813
Albert Cervera i Areny <albert@nan-tic.com>
2010-03-22 11:37:55 +0100

    Added nan_mrp_production_merge and nan_mrp_production_split modules that add actions for grouping and splitting production orders.

---
+++ nan_mrp_production_merge/__terp__.py
@@ -0,0 +1,18 @@
+{
+	"name" : "Production Merge",
+	"version" : "0.1",
+	"description" : """This module adds a new wizard that allows merging two or more production orders.""",
+	"author" : "NaN for Trod y Avia, S.L.",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 
+		'mrp',
+	],
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [ 
+		'mrp_view.xml',
+	],
+	"active": False,
+	"installable": True
+}
---
+++ nan_mrp_production_split/__terp__.py
@@ -0,0 +1,18 @@
+{
+	"name" : "Production Split",
+	"version" : "0.1",
+	"description" : """This module adds a new wizard that allows splitting a production order into two.""",
+	"author" : "NaN for Trod y Avia, S.L.",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 
+		'mrp',
+	],
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [ 
+		'mrp_view.xml',
+	],
+	"active": False,
+	"installable": True
+}
