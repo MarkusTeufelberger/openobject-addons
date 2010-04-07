4bbbdb67a346d02f61f1471c337e1b71d3cf6052
Albert Cervera i Areny <albert@nan-tic.com>
2010-04-07 04:22:10 +0200

    [ADD] Added two new modules 'nan_product_supplier_info_extended' which adds more information such as validity dates for supplier prices in products and nan_mrp_procurement_cheapest_supplier which will automatically select the cheapest supplier when creating purchase orders.

---
+++ nan_product_supplier_info_extended/__terp__.py
@@ -0,0 +1,14 @@
+{
+	"name" : "Product Supplier Info Extended",
+	"version" : "0.1",
+	"description" : """Product Supplier Info Extended""",
+	"author" : "NaN for Trod y Avia, S.L.",
+	"website" : "http://www.NaN-tic.com",
+	"depends" : [ 'product' ], 
+	"category" : "Custom Modules",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : [ 'product_view.xml' ],
+	"active": False,
+	"installable": True
+}
