46240fa4a64280c109f8dea851cfd459f0e80b80
Stephane Wirtel <stephane@openerp.com>
2010-06-11 12:38:50 +0200

    [FIX] Add the copyright of Raphael Valyi [FIX] Use the None constant instead of the dict on the stack [FIX] Give the context to the browse function [FIX] Use the browse_record object instead of a self.write function [REF] Remove the commented code [FIX] Add the help attribute on one field, to explain the behavior of this field

--- mrp_prodlot_autosplit/stock.py
+++ mrp_prodlot_autosplit/stock.py
@@ -1,3 +1,22 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2008 Raphaël Valyi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from osv import fields, osv
 import tools
 import ir
@@ -6,27 +25,27 @@ from tools.translate import _
 
 class stock_move(osv.osv):
     _inherit = "stock.move"
-    
-    def copy(self, cr, uid, id, default=None, context={}):
+ 
+    def copy(self, cr, uid, id, default=None, context=None):
         if not default:
             default = {}
         default['new_prodlot_code'] = False
         return super(stock_move, self).copy(cr, uid, id, default, context=context)
-    
-     
-    def _get_prodlot_code(self, cr, uid, ids, field_name, arg, context={}):
+
+
+    def _get_prodlot_code(self, cr, uid, ids, field_name, arg, context=None):
         res = {}
         for move in self.browse(cr, uid, ids):
             res[move.id] = move.prodlot_id and move.prodlot_id.name or False
         return res
-    
-    def _set_prodlot_code(self, cr, uid, ids, name, value, arg, context):
+
+    def _set_prodlot_code(self, cr, uid, ids, name, value, arg, context=None):
         if not value: return False
-        
+
         if isinstance(ids, (int, long)):
             ids = [ids]
-            
-        for move in self.browse(cr, uid, ids):
+
+        for move in self.browse(cr, uid, ids, context=context):
             product_id = move.product_id.id
             existing_prodlot = move.prodlot_id
             if existing_prodlot: #avoid creating a prodlot twice
@@ -36,40 +55,24 @@ class stock_move(osv.osv):
                     'name': value,
                     'product_id': product_id,
                 })
-                self.write(cr, uid, ids, {'prodlot_id': prodlot_id})
-            
+                move.write({'prodlot_id' : prodlot_id})
 
-    _columns = {        
+    _columns = {
         'new_prodlot_code': fields.function(_get_prodlot_code, fnct_inv=_set_prodlot_code,
-                 method=True, type='char', size=64, string='Production Tracking Code To Create', select=1),
+                                            method=True, type='char', size=64,
+                                            string='Production Tracking Code To Create', select=1
+                                           ),
     }
-    
-
-#    def _check_unique_product_lot(self, cr, uid, ids):
-#        print "########"
-#        for move in self.browse(cr, uid, ids):
-#            if move.state == 'done' and move.product_id.unique_production_number and move.product_qty > 1 and (\
-#                (move.product_id.track_production and move.location_id.usage == 'production') or \
-#                (move.product_id.track_production and move.location_dest_id.usage == 'production') or \
-#                (move.product_id.track_incoming and move.location_id.usage == 'supplier') or \
-#                (move.product_id.track_outgoing and move.location_dest_id.usage == 'customer')):
-#                return False
-#        return True
-#        
-#    _constraints = [
-#        (_check_unique_product_lot,
-#            """_(The product associated to the move require a unique (per instance) production number, 
-#            you should split the move assign a different number to every move)""",
-#            ['prodlot_id'])]
 
     def action_done(self, cr, uid, ids, context=None):
-        """If we autosplit moves without reconnecting them 1 by 1, at least when some move which has descendants is split
+        """
+        If we autosplit moves without reconnecting them 1 by 1, at least when some move which has descendants is split
         The following situation would happen (alphabetical order is order of creation, initially b and a pre-exists, then a is split, so a might get assigned and then split too):
         Incoming moves b, c, d
         Outgoing moves a, e, f
         Then we have those links: b->a, c->a, d->a
         and: b->, b->e, b->f
-        The following code will detect this situation and reconnect properly the moves into only: b->a, c->e and d->f  
+        The following code will detect this situation and reconnect properly the moves into only: b->a, c->e and d->f
         """
         result = super(stock_move, self).action_done(cr, uid, ids, context)
         for move in self.browse(cr, uid, ids):
@@ -85,7 +88,7 @@ class stock_move(osv.osv):
                         cr.execute("select stock_move.id from stock_move_history_ids left join stock_move on stock_move.id = stock_move_history_ids.parent_id where child_id=%s and stock_move.product_qty=1", (out_node,))
                         unitary_in_moves = cr.fetchall()
                         counter += 1
-                    
+
                     if len(unitary_in_moves) == len(unitary_out_moves):
                         unitary_out_moves.reverse()
                         unitary_out_moves.pop()
@@ -98,32 +101,34 @@ class stock_move(osv.osv):
                             counter += 1
 
         return result
-    
+
 stock_move()
 
 
 class stock_picking(osv.osv):
     _inherit = "stock.picking"
-    
+
     def action_assign_wkf(self, cr, uid, ids):
         result = super(stock_picking, self).action_assign_wkf(cr, uid, ids)
-        
+
         for picking in self.browse(cr, uid, ids):
-                for move in picking.move_lines:
-                    if move.product_id.unique_production_number and move.product_qty > 1 and (\
-                    (move.product_id.track_production and move.location_id.usage == 'production') or \
+            for move in picking.move_lines:
+                if move.product_id.unique_production_number and \
+                   move.product_qty > 1 and \
+                   ((move.product_id.track_production and move.location_id.usage == 'production') or \
                     (move.product_id.track_production and move.location_dest_id.usage == 'production') or \
                     (move.product_id.track_incoming and move.location_id.usage == 'supplier') or \
                     (move.product_id.track_outgoing and move.location_dest_id.usage == 'customer')):
 
                         qty = move.product_qty
                         self.pool.get('stock.move').write(cr, uid, move.id, {'product_qty': 1, 'product_uos_qty': move.product_id.uos_coeff})
+
                         while qty > 1:
                             self.pool.get('stock.move').copy(cr, uid, move.id, {'state': move.state, 'prodlot_id': None})
-                            qty -= 1;    
+                            qty -= 1;
 
         return result
-        
+
 stock_picking()
 
 
@@ -136,23 +141,28 @@ class stock_production_lot(osv.osv):
         highest number of products with the given serial (should be 1 if no mistake). This
         is better than using move dates because moves can easily be encoded at with wrong dates."""
         res = {}
-        
+
         for prodlot_id in ids:
-            cr.execute("select location_dest_id from stock_move inner join stock_report_prodlots \
-            on stock_report_prodlots.location_id = location_dest_id and stock_report_prodlots.prodlot_id = %s\
-            where stock_move.prodlot_id = %s and stock_move.state='done' \
-            order by stock_report_prodlots.name DESC" % (prodlot_id, prodlot_id))
+            cr.execute(
+                "select location_dest_id " \
+                "from stock_move inner join stock_report_prodlots on stock_report_prodlots.location_id = location_dest_id and stock_report_prodlots.prodlot_id = %s " \
+                "where stock_move.prodlot_id = %s and stock_move.state=%s "\
+                "order by stock_report_prodlots.name DESC ",
+                (prodlot_id, prodlot_id, 'done'))
             results = cr.fetchone()
 
-            if results and len(results) > 0:
-                res[prodlot_id] = results[0]#TODO return tuple to avoid name_get being requested by the GTK client
-            else:
-                res[prodlot_id] = False
+            #TODO return tuple to avoid name_get being requested by the GTK client
+            res[prodlot_id] = results and results[0] or False
 
         return res
-    
+
     _columns = {
-        'last_location_id': fields.function(_last_location_id, method=True, type="many2one", relation="stock.location", string="Last Location"),
+        'last_location_id': fields.function(_last_location_id, method=True,
+                                            type="many2one", relation="stock.location",
+                                            string="Last Location",
+                                            help="The last location where the product with given serial is"),
     }
 
 stock_production_lot()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
