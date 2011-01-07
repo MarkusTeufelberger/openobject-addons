a8f082bda935a9bb2f4b2ff0eae2f6ac271f1055
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2011-01-07 13:14:38 +0100

    [ADD] c2c_sale_bom_split to split product according to BoM in SO related packing

---
+++ c2c_sale_bom_split/__init__.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2010 Camptocamp SA (http://www.camptocamp.com) 
+# All Right Reserved
+#
+##############################################################################
+
+import c2c_sale_bom_split
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
